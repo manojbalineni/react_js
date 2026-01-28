c1aa613419af2f4af00f81e6de9e0796e0719fe3
Manoj <manoj.balineni04@gmail.com>
2026-01-28 12:16:15 +0530

    Added Cards

--- 01_React_Intro/app.js
+++ 01_React_Intro/app.js
@@ -1,11 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/cutlery-coming-out-from-chef-hat-3947ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestroCard = () => {
+  return (
+    <div className="restro-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="image-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/735b4a81-4bd2-4bdd-a606-be5fd03d9890_11094.JPG"
+        alt=""
+      />
+      <h3>Pizza Hut</h3>
+      <h4>Rating : 4.3</h4>
+      <h4>25-30 minutes</h4>
+      <h3>Pizzas</h3>
+    </div>
+  );
+};
+
+const BodyCmponent = () => {
+  return (
+    <div className="body">
+      <div className="search-input">Search Box</div>
+
+      <div className="restaurant-component">
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <BodyCmponent />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+/*
+
 const TitleComponent = () => {
   return <h1> This is hello from the Title Component</h1>;
 };
 
-/* Component Composition */
+Component Composition 
 const HeadingComponent = () => {
   return (
     <div id="heading">
@@ -18,6 +89,8 @@ const HeadingComponent = () => {
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
+
+*/
 
 /*
 

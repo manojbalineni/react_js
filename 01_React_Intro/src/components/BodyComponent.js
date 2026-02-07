ae03717653eb72a3a2cc7221d6b3f5ccb63d9745
Manoj <manoj.balineni04@gmail.com>
2026-02-07 17:10:47 +0530

    Added Hooks

--- 01_React_Intro/src/components/BodyComponent.js
+++ 01_React_Intro/src/components/BodyComponent.js
@@ -1,13 +1,25 @@
 import RestroCard from "./RestroCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const BodyCmponent = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
   return (
     <div className="body">
-      <div className="search-input">Search Box</div>
-
+      <div className="filter-btn">
+        <button
+          onClick={() => {
+            const filteredRestaurants = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4.5,
+            );
+            setListOfRestaurant(filteredRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-component">
-        {resList.map((restaurant) => (
+        {listOfRestaurant.map((restaurant) => (
           <RestroCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
